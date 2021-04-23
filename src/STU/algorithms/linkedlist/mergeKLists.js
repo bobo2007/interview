d989ccd4abb95c7b02246d751d3f02c543dd6746
yanxbm <yanxbm@yonyou.com>
2021-04-23 19:46:31 +0800

    test

--- src/STU/algorithms/linkedlist/mergeKLists.js
+++ src/STU/algorithms/linkedlist/mergeKLists.js
@@ -16,8 +16,33 @@
  * @param {ListNode[]} lists
  * @return {ListNode}
  */
-var mergeKLists = function(lists) {
-
+// 顺序合并 时间复杂度为O(k^2n), 空间复杂度O(1)
+var mergeKLists1 = function(lists) {
+    let result = null;
+    for(let i=0; i < lists.length; i++){
+        result = mergeTwoLists(result, lists[i]);
+    }
+    return result;
 };
+
+function mergeTwoLists(l1, l2){
+    if(l1 === null){
+        return l2;
+    }else if(l2 === null){
+        return l1;
+    }else if(l1.val < l2.val){
+        l1.next = mergeTwoLists(l1.next, l2);
+        return l1;
+    }else{
+        l2.next = mergeTwoLists(l1, l2.next);
+        return l2;
+    }
+}
+
+
+// 分治合并
+var mergeKLists = function(lists){
+    
+}
 // @lc code=end
 
