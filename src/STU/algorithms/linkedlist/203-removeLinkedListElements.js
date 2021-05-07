a9c696e1200cd7826268bdc705cedd5589ecaa90
yanxbm <yanxbm@yonyou.com>
2021-05-07 20:07:26 +0800

    删除链表元素

--- src/STU/algorithms/linkedlist/203-removeLinkedListElements.js
+++ src/STU/algorithms/linkedlist/203-removeLinkedListElements.js
@@ -17,8 +17,46 @@
  * @param {number} val
  * @return {ListNode}
  */
-var removeElements = function(head, val) {
-
+// 迭代  使用虚拟头节点
+var removeElements1 = function(head, val) {
+    let dummy = new ListNode(null, head);
+    let cur = dummy;
+    while(cur && cur.next){
+        if(cur.next.val == val){
+            cur.next = cur.next.next;
+        }else{
+            cur = cur.next;
+        }
+    }
+    return dummy.next;
 };
+
+// 迭代  删除头节点时单独考虑
+var removeElements2 = function(head, val) {
+    while(head && head.val == val){
+        head = head.next;
+    }
+    if(head == null) return null;
+    let cur = head;
+    while(cur.next){
+        if(cur.next.val == val){
+            cur.next = cur.next.next;
+        }else{
+            cur = cur.next;
+        }
+    }
+    return head;
+}
+
+// 递归  
+var removeElements = function(head, val) {
+    if(head == null) return null;
+    head.next = removeElements(head.next, val);
+    if(head.val == val) {
+        return head.next;
+    }else{
+        return head;
+    }
+}
 // @lc code=end
 
