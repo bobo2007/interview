f2a575fffded0196a9e0e8eb83865ad4b256ac99
yanxbm <yanxbm@yonyou.com>
2022-02-26 16:17:56 +0800

    test

--- src/STU/shouxie/bind.js
+++ src/STU/shouxie/bind.js
@@ -3,6 +3,7 @@
  * @Author: yanxbm
  * @Date: 2022-02-14 17:06:59
  * @LastEditors: yanxbm
- * @LastEditTime: 2022-02-14 17:07:00
+ * @LastEditTime: 2022-02-15 09:57:38
  */
 
+// 要点  1.返回一个新的函数 2. 新的函数可以预置参数
--- src/STU/shouxie/call_apply.js
+++ src/STU/shouxie/call_apply.js
@@ -1,6 +1,8 @@
 // "use strict";
-// 模拟实现call
-// 要点： 1. 第一个参数为空或null或undefined以普通函数形式调用(而不是以方法形式)  
+// 模拟实现call  考虑基于函数扩展原型方法
+
+// 要点： 
+// 1. 第一个参数为空或null或undefined以普通函数形式调用(而不是以方法形式)  
 // 2. 第一个参数为原始值 
 // 3. 如何不覆盖对象属性  
 // 4. 执行完后删除方法属性
@@ -17,16 +19,21 @@ Function.prototype.myCall = function(){
 		return this(...params);
 	}
 
-	let contextType = typeof context;
+	// let contextType = typeof context;
 
 	// 严格模式和非严格模式下都会将原始类型转换成对应的包装类型
-	if(contextType === "number"){
-		context = new Number(context);
-	}else if(contextType === "string"){
-		context = new String(context);
-	}else if(contextType == "boolean"){
-		context = new Boolean(context);
-	}
+	// if(contextType === "number"){
+	// 	context = new Number(context);
+	// }else if(contextType === "string"){
+	// 	context = new String(context);
+	// }else if(contextType == "boolean"){
+	// 	context = new Boolean(context);
+	// }
+
+	// 以上代码可以如下形式替换
+	// context = new Object(context); 	// Object 构造函数也会像工厂方法一样，根据传入值的类型返回相应基本包装类型的实例
+	// 或者
+	context = new context.constructor(context);
 
 	// 将目标函数添加到context对象，作为context对象的属性方法调用。
 	const fn = Symbol("fn"); // 使用Symbol不覆盖对象的原有属性
@@ -52,7 +59,9 @@ test.myCall(obj); // call  5
 
 
 // 模拟实现 apply  类似 call 只是接受参数为数组
-// 要点： 1.  第二个参数为数组  
+
+// 要点： 
+// 1. 第二个参数为数组  
 // 2. 实际调用时不传第二个参数或第二个参数不是数组
 Function.prototype.myApply = function(context, args){
 	// 判断第二个参数是否为数组, 非数组默认为空数组
@@ -60,14 +69,19 @@ Function.prototype.myApply = function(context, args){
 	if(context == null || context == undefined){
 		return this(...params);
 	}
-	let contextType = typeof context;
-	if(contextType === 'number'){
-		context = new Number(context);
-	}else if(contextType === 'boolean'){
-		context = new Boolean(context);
-	}else if(contextType === 'string'){
-		context = new String(context);
-	}
+	// let contextType = typeof context;
+
+	// if(contextType === 'number'){
+	// 	context = new Number(context);
+	// }else if(contextType === 'boolean'){
+	// 	context = new Boolean(context);
+	// }else if(contextType === 'string'){
+	// 	context = new String(context);
+	// }
+	
+	// 以上代码可以用下面的2种方式替换
+	// context = new Object(context); 或者如下
+	context = new context.constructor(context);
 
 	let fn = Symbol("fn");
 	context[fn] = this;
